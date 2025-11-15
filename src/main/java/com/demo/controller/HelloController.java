8c29c548e9c7b774641556750e85798f98b96c16
sanjeev51197@gmail.com <bbhanupriya520@gmail.com>
2025-11-15 16:42:43 +0530

    demo-app

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return "Hello Sanjeev";
+	}
+
+}
